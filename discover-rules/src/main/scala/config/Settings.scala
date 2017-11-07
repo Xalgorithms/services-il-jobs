181f9dd175cd8fe20f500460fa8b6ee6ed843537
Hayk <haykp@aobyte.com>
2017-11-07 04:06:13 +0400

    Push document_id:rule_id pairs kafka topic
    Change-Id: Icc10f216ad4f0a36cb816666b2789863aed9abc0

--- discover-rules/src/main/scala/config/Settings.scala
+++ discover-rules/src/main/scala/config/Settings.scala
@@ -6,6 +6,7 @@ object Settings {
   private val config = ConfigFactory.load()
 
   lazy val brokers = String.join(";", config.getStringList("brokers"))
-  lazy val topic =  config.getString("topic")
+  lazy val receiver_topic =  config.getString("receiver_topic")
+  lazy val producer_topic =  config.getString("producer_topic")
   lazy val cassandra_host =  config.getString("cassandra_host")
 }
