e294b3d73668bd5aa3d2cb72e8aeaa13dadbbaac
Hayk <haykp@aobyte.com>
2017-11-09 16:28:50 +0400

    Modify function chain
    Change-Id: I560b910cb0c427af0b3f669483188fdcd7f71f28

--- discover-items/src/main/scala/org/xalgorithms/discover_items/Job.scala
+++ discover-items/src/main/scala/org/xalgorithms/discover_items/Job.scala
@@ -9,8 +9,13 @@ import kafka.serializer.StringDecoder
 import com.datastax.spark.connector.streaming._
 import com.datastax.spark.connector._
 
+import org.json4s._
+import org.json4s.native.Serialization._
+import org.json4s.native.Serialization
 
 object Job {
+
+
   def main(args: Array[String]) {
     val sc = getSparkContext("DiscoverRulesJob")
     val batchDuration = Seconds(4)
@@ -30,34 +35,32 @@ object Job {
     )
       .map(_._2)
       .transform({rdd =>
-        val documentRDD = rdd.map({ids =>
-          val document_id = ids.split(":")(0)
+        val documentRDD = rdd.map({id_pair =>
+          val document_id = id_pair.split(":")(0)
           Tuple1(document_id)
         })
 
-        val ruleRDD = rdd.map({ids =>
-          val rule_id = ids.split(":")(1)
+        val ruleRDD = rdd.map({id_pair =>
+          val rule_id = id_pair.split(":")(1)
           Tuple1(rule_id)
         })
 
         val items = documentRDD.joinWithCassandraTable("xadf", "invoices").select("items")
-        val filters = ruleRDD.joinWithCassandraTable("xadf", "rules").select("filters")
+        val rules = ruleRDD.joinWithCassandraTable("xadf", "rules").select("filters")
 
-        items.cartesian(filters)
-      })
-      .filter({tuple =>
-        // TODO: Filter
-        true
+        // TODO: Modify items based on rules
+        items
       })
       .foreachRDD({rdd =>
-        rdd.foreach({tuple =>
-          // Dealing with ((document_id, CssandraRow(items)), (rule_id, CassandraRow(rule)))
-          val items = tuple._1._2.getList[String]("items")
-          val rule_id = tuple._2._1
+        rdd.foreach({item =>
+          val res = Map(
+            "id" -> item._1._1,
+            "items" -> item._2.getString("items")
+          )
+          implicit val formats = Serialization.formats(NoTypeHints)
 
-          items.foreach({item =>
-            kafkaSink.value.send(Settings.producer_topic, item + "::" + rule_id)
-          })
+          val jsonStr = write(res)
+          kafkaSink.value.send(Settings.producer_topic, jsonStr)
         })
       })
 
