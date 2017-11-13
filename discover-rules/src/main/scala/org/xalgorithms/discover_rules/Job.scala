21912de44d3347a42b121fc11fd24dd1f87e5358
Hayk <haykp@aobyte.com>
2017-11-14 03:38:03 +0400

    Filter by envelope
    Change-Id: Ie161068f034b939248758e55e24698eaca4e6f29

--- discover-rules/src/main/scala/org/xalgorithms/discover_rules/Job.scala
+++ discover-rules/src/main/scala/org/xalgorithms/discover_rules/Job.scala
@@ -9,7 +9,27 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
 import com.datastax.spark.connector.streaming._
 import com.datastax.spark.connector._
 
+
 object Job {
+  def traverse(source: UDTValue, path: Array[String], value: String): Boolean = {
+    val p = path.head
+
+    if (!source.columnNames.contains(p)) {
+      return true
+    }
+
+    if (path.length == 1) {
+      return source.getString(p) == value
+    }
+
+    val next = source.get[Option[UDTValue]](p)
+    if (next.isEmpty) {
+      return true
+    }
+
+    traverse(next.get, path.drop(1), value)
+  }
+
   def main(args: Array[String]) {
     val sc = getSparkContext("DiscoverRulesJob")
     val batchDuration = Seconds(4)
@@ -28,18 +48,39 @@ object Job {
       ssc, kafkaReceiverParams, Set(Settings.receiver_topic)
     ).map(_._2).map(Tuple1(_))
 
-    val rules = sc.cassandraTable("xadf", "rules")
+
 
     ids.joinWithCassandraTable("xadf", "invoices").map(_._2).transform({rdd =>
+      val rules = sc.cassandraTable("xadf", "rules")
       rdd.cartesian(rules)
-    }).filter({tuple =>
-      // TODO: Filter rules based on envelope
-      true
-    }).foreachRDD { rdd =>
-      rdd.foreach { t =>
-        kafkaSink.value.send(Settings.producer_topic, t._1.getString("id") + ":" + t._2.getString("id"))
+    }).transform({rdd =>
+      rdd.filter({tuple =>
+        val filters = tuple._2.get[Option[UDTValue]]("filters")
+
+        filters.isDefined
+      }).filter({tuple =>
+        val filters = tuple._2.getUDTValue("filters")
+        val envelope = filters.get[Option[Set[UDTValue]]]("envelope")
+
+        envelope.isDefined
+      }).filter({tuple =>
+        val filters = tuple._2.getUDTValue("filters")
+        val envelope = filters.get[List[UDTValue]]("envelope")
+
+        val path = envelope.head.getString("path")
+        val value = envelope.head.getString("value")
+
+        val path_steps = path.split("\\.")
+
+        traverse(tuple._1.getUDTValue(path_steps.head), path_steps.drop(1), value)
+      })
+    })
+      .foreachRDD { rdd =>
+        rdd.foreach { t =>
+          kafkaSink.value.send(Settings.producer_topic, t._1.getString("id") + ":" + t._2.getString("id"))
+        }
       }
-    }
+
     ssc.start()
     ssc.awaitTermination()
   }
