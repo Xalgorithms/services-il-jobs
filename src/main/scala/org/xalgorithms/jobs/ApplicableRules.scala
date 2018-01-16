0165c2cc75a477ac4ebf4b8d4f90dadf0bfe781c
Don Kelly <karfai@gmail.com>
2018-01-16 10:19:25 +0400

    Adds a second job to the JAR
    Change-Id: I3cd0dbe041eb3e2c92630a795ed70bb35cd03e39

--- src/main/scala/org/xalgorithms/jobs/ApplicableRules.scala
+++ src/main/scala/org/xalgorithms/jobs/ApplicableRules.scala
@@ -2,7 +2,7 @@ package org.xalgorithms.jobs
 
 import org.xalgorithms.apps._
 
-class EffectiveRules(cfg: ApplicationConfig) extends KafkaStreamingApplication(cfg) {
+class ApplicableRules(cfg: ApplicationConfig) extends KafkaStreamingApplication(cfg) {
   def execute(): Unit = {
     with_context(cfg, { (ctx, sctx, input) =>
       input.map { s => s.reverse }
@@ -10,9 +10,9 @@ class EffectiveRules(cfg: ApplicationConfig) extends KafkaStreamingApplication(c
   }
 }
 
-object EffectiveRules {
+object ApplicableRules {
   def main(args: Array[String]) : Unit = {
-    val job = new EffectiveRules(ApplicationConfig("EffectiveRules"))
+    val job = new ApplicableRules(ApplicationConfig("ApplicableRules"))
     job.execute()
   }
 }
