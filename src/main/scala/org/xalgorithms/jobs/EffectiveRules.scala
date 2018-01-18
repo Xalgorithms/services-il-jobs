640b51142df4b02084abfddbf6059df4d8124f0e
Don Kelly <karfai@gmail.com>
2018-01-18 22:12:14 +0400

    Finishes the filtering code
    This is the finished reimplementation of the filtering for the
    EffectiveRules job. More testing is required, but this should be very
    close to the final version.
    
    Change-Id: I7b0f152b1e3b38ffd8b6b75521c53d1f9d569379

--- src/main/scala/org/xalgorithms/jobs/EffectiveRules.scala
+++ src/main/scala/org/xalgorithms/jobs/EffectiveRules.scala
@@ -3,13 +3,13 @@ package org.xalgorithms.jobs
 import org.apache.spark.streaming.dstream._
 import com.datastax.spark.connector._
 import com.datastax.spark.connector.streaming._
-import org.joda.time.DateTime
+import org.joda.time.{ DateTime, DateTimeZone }
 
 import org.xalgorithms.apps._
 
 class Envelope(
-  country: Option[String], region: Option[String],
-  timezone: Option[String], issued: Option[DateTime]) extends Serializable {
+  val country: Option[String], val region: Option[String],
+  val timezone: Option[String], val issued: Option[DateTime]) extends Serializable {
 }
 
 object Envelope {
@@ -23,10 +23,62 @@ object Envelope {
 }
 
 class Effective(
-  country: Option[String], region: Option[String], timezone: Option[String],
-  starts: Option[DateTime], ends: Option[DateTime]) extends Serializable {
+  val country: Option[String], val region: Option[String], val timezone: Option[String],
+  val starts: Option[DateTime], val ends: Option[DateTime]) extends Serializable {
+
   def matches(e: Envelope): Boolean = {
-    return true
+    is_global_or_matching_jurisdication(e.country, e.region) &&
+      is_within_effective_period(e.issued, e.timezone);
+  }
+
+  def is_global_or_matching_jurisdication(
+    country: Option[String], region: Option[String]): Boolean = {
+    if (None == this.country) {
+      // this rule applies anywhere
+      true
+    } else if (this.country == Some(this.country) && None == this.region) {
+      // this rule applies anywhere in the country
+      this.country.get == country.get
+    }
+
+    // otherwise, must have matching region
+    this.country.get == country.get && this.region.get == region.get
+  }
+
+  def is_within_effective_period(
+    issued: Option[DateTime], timezone: Option[String]): Boolean = {
+
+    if (None == issued || None == timezone || None == this.timezone || (timezone.get != this.timezone.get)) {
+      // we're in different timezones, if the rule was created correctly, there should
+      // be an Effective for the correct timezone.
+      // in the case of missing zones, there's nothing we can do
+      // same for if the document has no timezone
+      false
+    }
+
+    val local_issued = make_local_datetime(issued.get, timezone.get)
+    val local_starts = maybe_make_local_datetime(this.starts, this.timezone.get)
+    val local_ends = maybe_make_local_datetime(this.ends, this.timezone.get)
+
+    val is_after = (None == local_starts) ||
+      local_issued.isEqual(local_starts.get) ||
+      local_issued.isAfter(local_starts.get)
+    val is_before = (None == local_ends) ||
+      local_issued.isEqual(local_ends.get) ||
+      local_issued.isBefore(local_ends.get)
+
+    is_after && is_before
+  }
+
+  def make_local_datetime(dt: DateTime, tz: String): DateTime = {
+    new DateTime(
+      dt.year.get, dt.monthOfYear.get, dt.dayOfMonth.get,
+      dt.hourOfDay.get, dt.minuteOfHour.get, dt.secondOfMinute.get, dt.millisOfSecond.get,
+      DateTimeZone.forID(tz))
+  }
+
+  def maybe_make_local_datetime(dt: Option[DateTime], tz: String): Option[DateTime] = {
+    if (Some(dt) == dt) Some(make_local_datetime(dt.get, tz)) else None
   }
 }
 
