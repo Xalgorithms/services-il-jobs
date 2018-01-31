de377414b31d2d7f8c94694763ef8209051a9e7b
Hayk <hayk.pilos@gmail.com>
2018-01-31 16:09:54 +0400

    Cleanup unit tests
    Change-Id: I15c560a89d0dc6ea836a6103937255ff19fe3898

--- src/test/scala/org/xalgorithms/rule_interpreter/interpreterTest.scala
+++ src/test/scala/org/xalgorithms/rule_interpreter/interpreterTest.scala
@@ -2,9 +2,6 @@ package org.xalgorithms.rule_interpreter
 
 
 import org.scalatest.{BeforeAndAfterEach, FunSuite}
-import play.api.libs.json.Json
-
-import scala.io.Source
 
 class interpreterTest extends FunSuite with BeforeAndAfterEach {
 
@@ -16,194 +13,30 @@ class interpreterTest extends FunSuite with BeforeAndAfterEach {
 
   }
 
-  test("Should interpret map1 properly") {
-    val contextSource = Source.fromURL(getClass.getResource("/context.json"))
-    val context = new Context(contextSource.mkString)
+  test("Should interpret map properly") {
+    testHelper.load("map", (c, steps, expected) =>{
+      val actualContext = interpreter.runAll(c, steps).get
+      val actual$ = (actualContext \ "$").get
 
-    val stepsSource = Source.fromURL(getClass.getResource("/map1.json"))
-    val steps = new Steps(stepsSource.mkString)
-
-    val actualContext = interpreter.runAll(context, steps).get
-    val actual$ = (actualContext \ "$").get
-
-    val expected$ = Json.obj(
-      "a" -> "text 1"
-    )
-
-    assert(actual$ == expected$)
+      assert(actual$ == expected)
+    })
   }
 
-  test("Should interpret map2 properly") {
-    val contextSource = Source.fromURL(getClass.getResource("/context.json"))
-    val context = new Context(contextSource.mkString)
+  test("Should interpret revise properly") {
+    testHelper.load("revise", (c, steps, expected) =>{
+      val actualContext = interpreter.runAll(c, steps).get
+      val actual$ = (actualContext \ "revision").get
 
-    val stepsSource = Source.fromURL(getClass.getResource("/map2.json"))
-    val steps = new Steps(stepsSource.mkString)
-
-    val actualContext = interpreter.runAll(context, steps).get
-    val actual$ = (actualContext \ "$").get
-
-    val expected$ = Json.obj(
-      "a" ->"text 1",
-      "b" -> "text 2",
-      "c" -> 2,
-      "d" -> "5"
-    )
-
-    assert(actual$ == expected$)
+      assert(actual$ == expected)
+    })
   }
 
-  test("Should interpret map3 properly") {
-    val contextSource = Source.fromURL(getClass.getResource("/context.json"))
-    val context = new Context(contextSource.mkString)
+  test("Should interpret assemble properly") {
+    testHelper.load("assemble", (c, steps, expected) =>{
+      val actualContext = interpreter.runAll(c, steps).get
+      val actual$ = (actualContext \ "table").get
 
-    val stepsSource = Source.fromURL(getClass.getResource("/map3.json"))
-    val steps = new Steps(stepsSource.mkString)
-
-    val actualContext = interpreter.runAll(context, steps).get
-    val actual$ = (actualContext \ "$").get
-
-    val expected$ = Json.obj(
-      "b" -> "1",
-      "a" -> 12
-    )
-
-    assert(actual$ == expected$)
-  }
-
-  test("Should interpret revise1 properly") {
-    val contextSource = Source.fromURL(getClass.getResource("/context.json"))
-    val context = new Context(contextSource.mkString)
-
-    val stepsSource = Source.fromURL(getClass.getResource("/revise1.json"))
-    val steps = new Steps(stepsSource.mkString)
-
-    val actualContext = interpreter.runAll(context, steps).get
-    val actual$ = (actualContext \ "revision").get
-
-    val expected$ = Json.obj(
-      "x" -> Json.obj(
-        "y" -> Json.obj(
-          "z" -> "text 1",
-          "w" -> "1"
-        )
-      ),
-      "p" -> Json.obj(
-        "q" -> "text 2"
-      ),
-      "a" -> "text 1",
-      "b" -> "text 2",
-      "c" -> 2,
-      "d" -> "s"
-    )
-
-    assert(actual$ == expected$)
-  }
-
-  test("Should interpret revise2 properly") {
-    val contextSource = Source.fromURL(getClass.getResource("/context.json"))
-    val context = new Context(contextSource.mkString)
-
-    val stepsSource = Source.fromURL(getClass.getResource("/revise2.json"))
-    val steps = new Steps(stepsSource.mkString)
-
-    val actualContext = interpreter.runAll(context, steps).get
-    val actual$ = (actualContext \ "revision").get
-
-    val expected$ = Json.obj(
-      "x" -> Json.obj(
-        "y" -> Json.obj(
-          "z" -> "text 1",
-          "w" -> "1"
-        )
-      ),
-      "p" -> Json.obj(
-        "q" -> "text 2"
-      ),
-      "a" -> 6
-    )
-
-    assert(actual$ == expected$)
-  }
-
-  test("Should interpret assemble1 properly") {
-    val contextSource = Source.fromURL(getClass.getResource("/context2.json"))
-    val context = new Context(contextSource.mkString)
-
-    val stepsSource = Source.fromURL(getClass.getResource("/assemble1.json"))
-    val steps = new Steps(stepsSource.mkString)
-
-    val actualContext = interpreter.runAll(context, steps).get
-    val actual$ = (actualContext \ "table").get
-
-    val expected$ = Json.obj(
-      "table1" -> Json.obj(
-        "a" -> Json.obj(
-          "b" -> "5"
-        ),
-        "c0" -> "7"
-      ),
-      "table0" -> Json.obj(
-        "c0" -> "7"
-      )
-    )
-
-    assert(actual$ == expected$)
-  }
-
-  test("Should interpret assemble2 properly") {
-    val contextSource = Source.fromURL(getClass.getResource("/context2.json"))
-    val context = new Context(contextSource.mkString)
-
-    val stepsSource = Source.fromURL(getClass.getResource("/assemble2.json"))
-    val steps = new Steps(stepsSource.mkString)
-
-    val actualContext = interpreter.runAll(context, steps).get
-    val actual$ = (actualContext \ "table").get
-
-    val expected$ = Json.obj(
-      "table1" -> Json.obj(
-        "a" -> Json.obj(
-          "b" -> "5"
-        ),
-        "c0" -> "7"
-      ),
-      "table0" -> Json.obj(
-        "nc0" -> "7"
-      )
-    )
-
-    assert(actual$ == expected$)
-  }
-
-  test("Should interpret assemble3 properly") {
-    val contextSource = Source.fromURL(getClass.getResource("/context3.json"))
-    val context = new Context(contextSource.mkString)
-
-    val stepsSource = Source.fromURL(getClass.getResource("/assemble3.json"))
-    val steps = new Steps(stepsSource.mkString)
-
-    val actualContext = interpreter.runAll(context, steps).get
-    val actual$ = (actualContext \ "table").get
-
-    val expected$ = Json.obj(
-      "table1" -> Json.obj(
-        "a" -> Json.obj(
-          "b" -> "5"
-        ),
-        "c0" -> "7",
-        "c1" -> "1"
-      ),
-      "table2" -> Json.obj(
-        "b0" -> "8"
-      ),
-      "table0" -> Json.obj(
-        "nc0" -> "7",
-        "nc1" -> "1",
-        "nc2" -> "8"
-      )
-    )
-
-    assert(actual$ == expected$)
+      assert(actual$ == expected)
+    })
   }
 }
---
+++ src/test/scala/org/xalgorithms/rule_interpreter/testHelper.scala
@@ -0,0 +1,28 @@
+package org.xalgorithms.rule_interpreter
+
+import play.api.libs.json.{JsObject, JsValue, Json}
+
+import scala.io.Source
+
+object testHelper {
+  def load(path: String, cb: (Context, Steps, JsValue) => Unit) = {
+    val source = Source.fromURL(getClass.getResource(s"/$path.json"))
+    val json = Json.parse(source.mkString).as[List[JsValue]]
+
+    json.foreach({j =>
+      val contextPath = (j \ "context").get.as[String]
+      val inputStr = Json.stringify((j \ "input" \ "steps").get)
+      val expected = (j \ "expects").get
+
+      val context = loadContext(contextPath)
+      val input = new Steps(inputStr)
+
+      cb(context, input, expected)
+    })
+  }
+
+  private[this] def loadContext(path: String): Context = {
+    val c = Source.fromURL(getClass.getResource(s"/contexts/$path.json"))
+    new Context(c.mkString)
+  }
+}
