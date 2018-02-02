5b4a01e9cce62d75f59753aaa3d6d95c3875b7a5
Don Kelly <karfai@gmail.com>
2018-02-03 03:05:52 +0400

    Completes sketch of ApplicableRules with comments
    This is a reworking of the ApplicableRules job implemented in
    discover-items. It works for some simple tests. More testing is
    required.
    
    Change-Id: I7d6ab96dce73d7e4eb94295ef958e7b53a1967fa

---
+++ src/test/scala/org/xalgorithms/bson/FindSpec.scala
@@ -0,0 +1,23 @@
+package org.xalgorithms.bson
+
+import org.xalgorithms.bson._
+
+import org.bson._
+import org.scalatest._
+
+class FindSpec extends FlatSpec with Matchers {
+  "Find" should "extract values from BsonDocument by path" in {
+    val JSON = """{
+      "a" : { "aa" : 1, "ab" : "2" },
+      "b" : "3",
+      "c" : 1
+    }"""
+
+    val doc = BsonDocument.parse(JSON)
+    Find(doc, "a") shouldBe BsonDocument.parse("""{ "aa" : 1, "ab" : "2" }""")
+    Find(doc, "a.aa") shouldBe new BsonInt32(1)
+    Find(doc, "a.ab") shouldBe new BsonString("2")
+    Find(doc, "b") shouldBe new BsonString("3")
+    Find(doc, "c") shouldBe new BsonInt32(1)
+  }
+}
