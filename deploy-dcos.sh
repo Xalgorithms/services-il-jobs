eb76aa594d94c599093d4e92f6e1c3b50afcaac1
Don Kelly <karfai@gmail.com>
2018-03-23 03:49:44 +0400

    Updates deployment scripting to include options
    Change-Id: Ic887694ae913008003398be9bfc1694cb4d50549

--- deploy-dcos.sh
+++ deploy-dcos.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 echo "> attempting: $1 (version: $2)"
-dcos spark run --name "spark" --submit-args="--driver-cores=0.5 --driver-memory=1024M --class org.xalgorithms.jobs.$1 --conf spark.jars.packages=datastax:spark-cassandra-connector:2.0.3-s_2.11,com.typesafe:config:1.3.1,org.apache.spark:spark-streaming-kafka-0-8_2.11:2.2.0,net.ceedubs:ficus_2.11:1.1.1,org.mongodb.spark:mongo-spark-connector_2.11:2.2.0 --conf spark.cassandra.connection.host=node-0-server.cassandra.autoip.dcos.thisdcos.directory https://github.com/Xalgorithms/xadf-jobs/releases/download/v$2/xa-spark-jobs_2.11-1.0.jar"
+OPTS=`cat $1.opts.production`
+echo ">   additional opts: $OPTS"
+
+dcos spark run --name "spark" --submit-args="--driver-cores=0.5 --driver-memory=1024M --class org.xalgorithms.jobs.$1 --conf spark.cores.max=2 --conf spark.jars.packages=datastax:spark-cassandra-connector:2.0.3-s_2.11,com.typesafe:config:1.3.1,org.apache.spark:spark-streaming-kafka-0-8_2.11:2.2.0,net.ceedubs:ficus_2.11:1.1.1,org.mongodb.spark:mongo-spark-connector_2.11:2.2.0,org.gnieh:diffson-play-json_2.11:2.2.1 $OPTS https://github.com/Xalgorithms/xadf-jobs/releases/download/v$2/xa-spark-jobs_2.11-1.0.jar"
--- local-spark-submit.sh
+++ local-spark-submit.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-spark-submit --class "org.xalgorithms.jobs.$1" --packages datastax:spark-cassandra-connector:2.0.3-s_2.11,com.typesafe:config:1.3.1,org.apache.spark:spark-streaming-kafka-0-8_2.11:2.2.0,net.ceedubs:ficus_2.11:1.1.1,org.mongodb.spark:mongo-spark-connector_2.11:2.2.0 target/scala-2.11/xa-spark-jobs_2.11-1.0.jar
+echo "> attempting: $1"
+OPTS=`cat $1.opts.development`
+echo ">   additional opts: $OPTS"
+
+spark-submit --class "org.xalgorithms.jobs.$1" --packages datastax:spark-cassandra-connector:2.0.3-s_2.11,com.typesafe:config:1.3.1,org.apache.spark:spark-streaming-kafka-0-8_2.11:2.2.0,net.ceedubs:ficus_2.11:1.1.1,org.mongodb.spark:mongo-spark-connector_2.11:2.2.0,org.gnieh:diffson-play-json_2.11:2.2.1 $OPTS target/scala-2.11/xa-spark-jobs_2.11-1.0.jar
