40950a56eeb52d4b3352a45ba743e3516aade537
Don Kelly <karfai@gmail.com>
2018-04-04 01:47:25 +0400

    Read REQUIRE, ASSEMBLE from JSON
    Change-Id: I506e2d4b4e1bb6c55dc9661aa395b456de389ae3

---
+++ src/main/scala/org/xalgorithms/rules/Syntax.scala
@@ -0,0 +1,224 @@
+package org.xalgorithms.rules
+
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+import scala.io.Source
+
+class Step {
+}
+
+class PackagedTableReference(val package_name: String, val id: String, val version: String, val name: String) {
+}
+
+class Column(val table: Reference, val sources: Seq[TableSource]) {
+}
+
+class TableSource(val whens: Seq[When]) {
+}
+
+class ColumnTableSource(val name: String, val source: String, whens: Seq[When]) extends TableSource(whens) {
+}
+
+class ColumnsTableSource(val columns: Seq[String], whens: Seq[When]) extends TableSource(whens) {
+}
+
+class Value {
+}
+
+class Reference(val section: String, val key: String) extends Value {
+}
+
+class Number(val value: Double) extends Value {
+}
+
+class StringValue(val value: String) extends Value {
+}
+
+class When(val left: Value, val right: Value, val op: String) {
+}
+
+class Assemble(val name: String, val columns: Seq[Column]) extends Step {
+}
+
+class Filter extends Step {
+}
+
+class Keep extends Step {
+}
+
+class MapStep extends Step {
+}
+
+class Reduce extends Step {
+}
+
+class Require(val table_reference: PackagedTableReference, val indexes: Seq[String]) extends Step {
+}
+
+class Revise extends Step {
+}
+
+object StepProduce {
+  implicit val columnReads: Reads[Column] = (
+    (JsPath \ "table").read[JsObject] and
+    (JsPath \ "sources").read[JsArray]
+  )(produce_column _)
+
+  implicit val referenceReads: Reads[Reference] = (
+    (JsPath \ "section").read[String] and
+    (JsPath \ "key").read[String]
+  )(produce_reference _)
+
+  implicit val colsSourceReads: Reads[ColumnsTableSource] = (
+    (JsPath \ "columns").read[JsArray] and
+    (JsPath \ "whens").read[JsArray]
+  )(produce_columns_table_source _)
+
+  implicit val colSourceReads: Reads[ColumnTableSource] = (
+    (JsPath \ "name").read[String] and
+    (JsPath \ "source").read[String] and
+    (JsPath \ "whens").read[JsArray]
+  )(produce_column_table_source _)
+
+  implicit val whenReads: Reads[When] = (
+    (JsPath \ "left").read[JsObject] and
+    (JsPath \ "right").read[JsObject] and
+    (JsPath \ "op").read[String]
+  )(produce_when _)
+
+  implicit val valueReads: Reads[Value] = (
+    (JsPath \ "type").read[String] and
+    (JsPath).read[JsObject]
+  )(produce_value _)
+
+  def stringOrNull(content: JsObject, k: String): String = {
+    return (content \ k).validate[String].getOrElse(null)
+  }
+
+  def doubleOrNull(content: JsObject, k: String): Double = {
+    var rv = null.asInstanceOf[Double]
+    val sv = stringOrNull(content, k)
+
+    if (null != sv) {
+      rv = sv.toDouble
+    }
+
+    return rv
+  }
+
+  def produce_packaged_table_reference(content: JsObject): PackagedTableReference = {
+    return new PackagedTableReference(
+      stringOrNull(content, "package"),
+      stringOrNull(content, "id"),
+      stringOrNull(content, "version"),
+      stringOrNull(content, "name")
+    )
+  }
+
+  def produce_columns_table_source(
+    columns: JsArray, whens: JsArray): ColumnsTableSource = {
+    return new ColumnsTableSource(
+      columns.validate[Seq[String]].getOrElse(Seq()),
+      whens.validate[Seq[When]].getOrElse(Seq())
+    )
+  }
+
+  def produce_column_table_source(
+    name: String, source: String, whens: JsArray): ColumnTableSource = {
+    return new ColumnTableSource(
+      name, source,
+      whens.validate[Seq[When]].getOrElse(Seq())
+    )
+  }
+
+  def produce_when(left: JsObject, right: JsObject, op: String): When = {
+    return new When(
+      left.validate[Value].getOrElse(null),
+      right.validate[Value].getOrElse(null),
+      op)
+  }
+
+  def produce_value(vt: String, content: JsObject): Value = {
+    if ("string" == vt) {
+      return new StringValue(stringOrNull(content, "value"))
+    } else if ("number" == vt) {
+      return new Number(doubleOrNull(content, "value"))
+    } else if ("reference" == vt) {
+      return new Reference(stringOrNull(content, "section"), stringOrNull(content, "key"))
+    }
+
+    return null
+  }
+
+  def produce_column(table_reference: JsObject, sources: JsArray): Column = {
+    return new Column(
+      table_reference.validate[Reference].getOrElse(null),
+      sources.validate[Seq[ColumnsTableSource]].getOrElse(Seq()) ++
+        sources.validate[Seq[ColumnTableSource]].getOrElse(Seq())
+    )
+  }
+
+  def produce_reference(section: String, key: String): Reference = {
+    return new Reference(section, key)
+  }
+
+  def produce_assemble(content: JsObject): Step = {
+    return new Assemble(
+      stringOrNull(content, "table_name"),
+      (content \ "columns").validate[Seq[Column]].getOrElse(null)
+    )
+  }
+
+  def produce_filter(content: JsObject): Step = {
+    return new Filter()
+  }
+
+  def produce_keep(content: JsObject): Step = {
+    return new Keep()
+  }
+
+  def produce_map(content: JsObject): Step = {
+    return new MapStep()
+  }
+
+  def produce_reduce(content: JsObject): Step = {
+    return new Reduce()
+  }
+
+  def produce_require(content: JsObject): Step = {
+    return new Require(
+      produce_packaged_table_reference(
+        (content \ "reference").as[JsObject]),
+      (content \ "indexes").as[Seq[String]])
+  }
+
+  def produce_revise(content: JsObject): Step = {
+    return new Revise()
+  }
+
+  val fns = Map[String, (JsObject) => Step](
+    "assemble" -> produce_assemble,
+    "filter" -> produce_filter,
+    "keep" -> produce_keep,
+    "map" -> produce_map,
+    "reduce" -> produce_reduce,
+    "require" -> produce_require,
+    "revise" -> produce_revise
+  )
+
+  def apply(name: String, content: JsObject): Step = {
+    return fns(name)(content)
+  }
+}
+
+object SyntaxFromSource {
+  implicit val stepReads: Reads[Step] = (
+    (JsPath \ "name").read[String] and
+    (JsPath).read[JsObject]
+  )(StepProduce.apply _)
+
+  def apply(source: Source): Seq[Step] = {
+    val res = (Json.parse(source.mkString) \ "steps").validate[Seq[Step]]
+    res.getOrElse(Seq())
+  }
+}
---
+++ src/test/scala/org/xalgorithms/rules/SyntaxSpec.scala
@@ -0,0 +1,124 @@
+package org.xalgorithms.rules
+
+import org.xalgorithms.rules._
+
+import scala.io.Source
+import org.scalatest._
+
+class SyntaxSpec extends FlatSpec with Matchers {
+  "Syntax" should "load Assemble from JSON" in {
+    val source = Source.fromURL(getClass.getResource("/assemble.json"))
+    val steps = SyntaxFromSource(source)
+    steps.length shouldBe 1
+    steps.head should not be null
+    steps.head shouldBe a [Assemble]
+
+    val o = steps.head.asInstanceOf[Assemble]
+    o.name shouldEqual("table_final")
+
+    o.columns.length shouldBe 2
+
+    o.columns(0).table should not be null
+    o.columns(0).table.section shouldEqual("tables")
+    o.columns(0).table.key shouldEqual("table0")
+    o.columns(0).sources.length shouldBe 1
+    o.columns(0).sources(0) should not be null
+    o.columns(0).sources(0) shouldBe a [ColumnsTableSource]
+    o.columns(0).sources(0).asInstanceOf[ColumnsTableSource].columns shouldEqual(Seq("c0", "c1", "c2"))
+    o.columns(0).sources(0).whens.length shouldBe 1
+    o.columns(0).sources(0).whens(0) should not be null
+    o.columns(0).sources(0).whens(0).left should not be null
+    o.columns(0).sources(0).whens(0).left shouldBe a [Reference]
+    o.columns(0).sources(0).whens(0).left.asInstanceOf[Reference].section shouldEqual("_context")
+    o.columns(0).sources(0).whens(0).left.asInstanceOf[Reference].key shouldEqual("a")
+    o.columns(0).sources(0).whens(0).right should not be null
+    o.columns(0).sources(0).whens(0).right shouldBe a [StringValue]
+    o.columns(0).sources(0).whens(0).right.asInstanceOf[StringValue].value shouldEqual("a distant ship")
+    o.columns(0).sources(0).whens(0).op shouldEqual("eq")
+
+    o.columns(1).table should not be null
+    o.columns(1).table.section shouldEqual("tables")
+    o.columns(1).table.key shouldEqual("table1")
+    o.columns(1).sources.length shouldBe 1
+    o.columns(1).sources(0) should not be null
+    o.columns(1).sources(0) shouldBe a [ColumnTableSource]
+    o.columns(1).sources(0).asInstanceOf[ColumnTableSource].name shouldEqual("y")
+    o.columns(1).sources(0).asInstanceOf[ColumnTableSource].source shouldEqual("x")
+    o.columns(1).sources(0).whens.length shouldBe 1
+    o.columns(1).sources(0).whens(0) should not be null
+    o.columns(1).sources(0).whens(0).left should not be null
+    o.columns(1).sources(0).whens(0).left shouldBe a [Reference]
+    o.columns(1).sources(0).whens(0).left.asInstanceOf[Reference].section shouldEqual("_local")
+    o.columns(1).sources(0).whens(0).left.asInstanceOf[Reference].key shouldEqual("x")
+    o.columns(1).sources(0).whens(0).right should not be null
+    o.columns(1).sources(0).whens(0).right shouldBe a [Number]
+    o.columns(1).sources(0).whens(0).right.asInstanceOf[Number].value shouldEqual(1.0)
+    o.columns(1).sources(0).whens(0).op shouldEqual("eq")
+  }
+
+  it should "load Filter from JSON" in {
+    val source = Source.fromURL(getClass.getResource("/filter.json"))
+    val steps = SyntaxFromSource(source)
+    steps.length shouldBe 1
+    steps.head should not be null
+    steps.head shouldBe a [Filter]
+
+    val o = steps.head.asInstanceOf[Filter]
+  }
+
+  it should "load Keep from JSON" in {
+    val source = Source.fromURL(getClass.getResource("/keep.json"))
+    val steps = SyntaxFromSource(source)
+    steps.length shouldBe 1
+    steps.head should not be null
+    steps.head shouldBe a [Keep]
+
+    val o = steps.head.asInstanceOf[Keep]
+  }
+
+  it should "load MapStep from JSON" in {
+    val source = Source.fromURL(getClass.getResource("/map.json"))
+    val steps = SyntaxFromSource(source)
+    steps.length shouldBe 1
+    steps.head should not be null
+    steps.head shouldBe a [MapStep]
+
+    val o = steps.head.asInstanceOf[MapStep]
+  }
+
+  it should "load Reduce from JSON" in {
+    val source = Source.fromURL(getClass.getResource("/reduce.json"))
+    val steps = SyntaxFromSource(source)
+    steps.length shouldBe 1
+    steps.head should not be null
+    steps.head shouldBe a [Reduce]
+
+    val o = steps.head.asInstanceOf[Reduce]
+  }
+
+  it should "load Require from JSON" in {
+    val source = Source.fromURL(getClass.getResource("/require.json"))
+    val steps = SyntaxFromSource(source)
+    steps.length shouldBe 1
+    steps.head should not be null
+    steps.head shouldBe a [Require]
+
+    val o = steps.head.asInstanceOf[Require]
+    o.table_reference should not be null
+    o.table_reference.package_name shouldEqual "package"
+    o.table_reference.id shouldEqual "id"
+    o.table_reference.version shouldEqual "1.2.34"
+    o.table_reference.name shouldEqual "table_name"
+    o.indexes shouldEqual Seq("a", "b")
+  }
+
+  it should "load Revise from JSON" in {
+    val source = Source.fromURL(getClass.getResource("/revise.json"))
+    val steps = SyntaxFromSource(source)
+    steps.length shouldBe 1
+    steps.head should not be null
+    steps.head shouldBe a [Revise]
+
+    val o = steps.head.asInstanceOf[Revise]
+  }
+}
