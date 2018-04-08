f9ac3535b6368a5b8ba23753510120b009ddc3e2
Don Kelly <karfai@gmail.com>
2018-04-08 20:22:11 +0400

    Factors MapStep assignment resolution to support functions
    Change-Id: Ie7e0e7499243127273ae6f9b14031d50fa634a1b

--- src/main/scala/org/xalgorithms/rules/steps/MapStep.scala
+++ src/main/scala/org/xalgorithms/rules/steps/MapStep.scala
@@ -6,18 +6,23 @@ import org.xalgorithms.rules.elements.{ Assignment, Reference, Value }
 class MapStep(table: Reference, assignments: Seq[Assignment]) extends AssignmentStep(table, assignments) {
   def execute(ctx: Context) {
     val tbl = ctx.find_in_section(table.section, table.key)
-    ctx.retain(table.section, table.key, tbl.map { r =>
-      r ++ assignments.foldLeft(Map[String, Value]()) { (o, ass) =>
-        val rs = ass.source.asInstanceOf[Reference]
+    ctx.retain(table.section, table.key, tbl.map { row =>
+      row ++ assignments.foldLeft(Map[String, Value]()) { (o, ass) =>
+        val av = resolve_assignment_source(row, ass.source)
 
-        if (rs.section == "_context") {
-          o ++ Map(ass.target -> r.getOrElse(rs.key, null))
+        if (null != av) {
+          o ++ Map(ass.target -> av)
         } else {
-          println("DEBT: only context sources are supported")
+          println("DEBT: unsupported or unknown source")
           o
         }
       }
     })
+  }
+
+  def resolve_assignment_source(row: Map[String, Value], v: Value): Value = v match {
+    case (rv: Reference) => if (rv.section == "_context") row.getOrElse(rv.key, null) else null
+    case _ => null
   }
 }
 
