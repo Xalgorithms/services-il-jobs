9cd42a50b88361152f8b2d9f1b47e35c58827510
Don Kelly <karfai@gmail.com>
2018-04-08 19:54:49 +0400

    Flatten hierarchies into a single keyspace when loading
    The tests for this SHOULD BE in LoadTableSource... There's is a debt
    marker to indicate this oversight.
    
    Change-Id: Ib57085688f0cd8f660860872f622d4613857d461

--- src/test/scala/org/xalgorithms/rules/steps/RequireStepSpec.scala
+++ src/test/scala/org/xalgorithms/rules/steps/RequireStepSpec.scala
@@ -31,4 +31,38 @@ class RequireSpec extends FlatSpec with Matchers {
     tbl(2)("b") shouldBe a [StringValue]
     tbl(2)("b").asInstanceOf[StringValue].value shouldEqual("baz")
   }
+
+  it should "flatten JSON hierarchies when loading" in {
+    val ctx = new Context(new ResourceLoadTableSource())
+
+    val ref = new PackagedTableReference("package", "table0", "0.0.1", "table_hier")
+    val step = new RequireStep(ref, Seq())
+
+    step.execute(ctx)
+
+    val tbl = ctx.find_in_section("table", "table_hier")
+
+    tbl should not be null
+    tbl.length shouldEqual(2)
+
+    tbl(0).exists(_._1 == "a.aa") shouldBe true
+    tbl(0)("a.aa") shouldBe a [StringValue]
+    tbl(0)("a.aa").asInstanceOf[StringValue].value shouldEqual("00")
+    tbl(0).exists(_._1 == "a.ab") shouldBe true
+    tbl(0)("a.ab") shouldBe a [StringValue]
+    tbl(0)("a.ab").asInstanceOf[StringValue].value shouldEqual("01")
+    tbl(0).exists(_._1 == "b") shouldBe true
+    tbl(0)("b") shouldBe a [StringValue]
+    tbl(0)("b").asInstanceOf[StringValue].value shouldEqual("02")
+
+    tbl(1).exists(_._1 == "a") shouldBe true
+    tbl(1)("a") shouldBe a [StringValue]
+    tbl(1)("a").asInstanceOf[StringValue].value shouldEqual("10")
+    tbl(1).exists(_._1 == "b.ba") shouldBe true
+    tbl(1)("b.ba") shouldBe a [StringValue]
+    tbl(1)("b.ba").asInstanceOf[StringValue].value shouldEqual("11")
+    tbl(1).exists(_._1 == "b.bb") shouldBe true
+    tbl(1)("b.bb") shouldBe a [StringValue]
+    tbl(1)("b.bb").asInstanceOf[StringValue].value shouldEqual("12")
+  }
 }
