cb99d9640947311e2011153b14120761d1430163
Don Kelly <karfai@gmail.com>
2018-04-23 10:39:24 +0400

    Factors out LoadJsonTableSource from LoadTableSource
    This will permit loading from BsonDocuments in LoadTableSource.
    
    Change-Id: I5e6620f747c014086bdb07cbcc0c08aca28a99ce

--- src/test/scala/org/xalgorithms/rules/steps/ResourceTableLoadSource.scala
+++ src/test/scala/org/xalgorithms/rules/steps/ResourceTableLoadSource.scala
@@ -1,12 +1,12 @@
 package org.xalgorithms.rules.steps
 
-import org.xalgorithms.rules.{ LoadTableSource }
+import org.xalgorithms.rules.{ LoadJsonTableSource }
 import org.xalgorithms.rules.elements.{ PackagedTableReference }
 
 import play.api.libs.json._
 import scala.io.Source
 
-class ResourceLoadTableSource extends LoadTableSource {
+class ResourceLoadTableSource extends LoadJsonTableSource {
   def read(ptref: PackagedTableReference): JsValue = {
     return Json.parse(Source.fromURL(getClass.getResource(s"/${ptref.name}.json")).mkString)
   }
