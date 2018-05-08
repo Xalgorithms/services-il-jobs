9263838c4ea51fa27493362aa49d044dd8f163b2
Don Kelly <karfai@gmail.com>
2018-05-08 18:31:58 +0400

    Updates Filter and Map steps to use new ResolveValue and related
    Change-Id: Ia52e342fa607ae4a6bad882f7d93d353aa322575

--- src/main/scala/org/xalgorithms/rules/steps/FilterStep.scala
+++ src/main/scala/org/xalgorithms/rules/steps/FilterStep.scala
@@ -1,6 +1,6 @@
 package org.xalgorithms.rules.steps
 
-import org.xalgorithms.rules.{ Context }
+import org.xalgorithms.rules.{ Context, RowContext }
 import org.xalgorithms.rules.elements.{ ReferenceValue, TableReference, When }
 
 class FilterStep(val table: TableReference, val filters: Seq[When]) extends Step {
@@ -8,23 +8,8 @@ class FilterStep(val table: TableReference, val filters: Seq[When]) extends Step
     val tbl = ctx.lookup_table(table.section, table.name)
 
     ctx.retain_table(table.section, table.name, tbl.filter { r =>
-      filters.foldLeft(false) { (v, wh) =>
-        val lr = wh.left.asInstanceOf[ReferenceValue]
-        val rr = wh.right.asInstanceOf[ReferenceValue]
-
-        if (null != lr && lr.section == "_context" && null != rr && rr.section == "_context") {
-          val lv = r.getOrElse(lr.key, null)
-          if (null != lv) {
-            lv.matches(r.getOrElse(rr.key, null), wh.op)
-          } else {
-            println("WARN: left value is not in the table")
-            false
-          }
-        } else {
-          println("DEBT: only contextual reference filters are supported")
-          false
-        }
-      }
+      val rctx = new RowContext(ctx, Map(), r)
+      filters.foldLeft(true) { (v, wh) => v && wh.evaluate(rctx) }
     })
   }
 }
--- src/main/scala/org/xalgorithms/rules/steps/MapStep.scala
+++ src/main/scala/org/xalgorithms/rules/steps/MapStep.scala
@@ -1,48 +1,20 @@
 package org.xalgorithms.rules.steps
 
-import org.xalgorithms.rules.{ Context }
+import org.xalgorithms.rules.{ Context, RowContext }
 import org.xalgorithms.rules.elements._
 
 class MapStep(table: TableReference, assignments: Seq[Assignment]) extends AssignmentStep(table, assignments) {
   def execute(ctx: Context) {
     val tbl = ctx.lookup_table(table.section, table.name)
     ctx.retain_table(table.section, table.name, tbl.map { row =>
-      row ++ assignments.foldLeft(Map[String, IntrinsicValue]()) { (o, ass) =>
-        val aov = resolve_to_atomic_value(row, ass.source)
-
-        aov match {
-          case Some(av) => o ++ Map(ass.target -> av)
+      val rctx = new RowContext(ctx, Map(), row)
+      assignments.foldLeft(row) { (o, ass) =>
+        ResolveValue(ass.source, rctx) match {
+          case Some(av) => o + (ass.target -> av)
           case None => o
         }
       }
     })
-  }
-
-  def resolve_to_atomic_value(row: Map[String, IntrinsicValue], v: Value): Option[IntrinsicValue] = v match {
-    case (rv: ReferenceValue) => if (rv.section == "_context") Some(row.getOrElse(rv.key, null)) else None
-    case (fv: FunctionValue) => apply(fv.name, fv.args.map(arg => resolve_to_atomic_value(row, arg)))
-    case (iv: IntrinsicValue) => Some(iv)
-    case _ => None
-  }
-
-  def apply(fn: String, args: Seq[Option[IntrinsicValue]]): Option[IntrinsicValue] = fn match {
-    case "add" => Some(apply_add(args))
-    case _ => None
-  }
-
-  def apply_add(args: Seq[Option[IntrinsicValue]]): IntrinsicValue = {
-    new NumberValue(args.foldLeft(BigDecimal(0.0)) { (sum, n) =>
-      n match {
-        case Some(v) => sum + make_number(v)
-        case None => sum
-      }
-    })
-  }
-
-  def make_number(v: Value): BigDecimal = v match {
-    case (nv: NumberValue) => nv.value
-    case (sv: StringValue) => BigDecimal(sv.value)
-    case _ => 0.0
   }
 }
 
