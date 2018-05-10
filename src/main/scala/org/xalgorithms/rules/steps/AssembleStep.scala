011623dd76428db232e26cd8d9265e8837ad02fd
Don Kelly <karfai@gmail.com>
2018-05-10 08:29:23 +0400

    Completes all COLUMN, COLUMNS combinations for AssembleStep
    Change-Id: I1c8ec7e45bc454b8f21171027cb911e5dbe65560

--- src/main/scala/org/xalgorithms/rules/steps/AssembleStep.scala
+++ src/main/scala/org/xalgorithms/rules/steps/AssembleStep.scala
@@ -1,33 +1,52 @@
 package org.xalgorithms.rules.steps
 
 import org.xalgorithms.rules.{ Context }
-import org.xalgorithms.rules.elements.{ Column, ColumnsTableSource, IntrinsicValue }
+import org.xalgorithms.rules.elements._
 
 class AssembleStep(val name: String, val columns: Seq[Column]) extends Step {
   def execute(ctx: Context) {
     val combined_tbl = columns.foldLeft(Seq[Map[String, IntrinsicValue]]()) { (tbl, col) =>
-      val src = col.sources.head
-      var stbl = Seq[Map[String, IntrinsicValue]]()
-
-      if (col.sources.length > 1) {
-        println("DEBT: only single column sources are supported")
+      col.sources.size match {
+        case 0 => tbl
+        case _ => combine(tbl, build_table_from_sources(ctx, col.table, col.sources))
       }
-
-      val cols_source = src.asInstanceOf[ColumnsTableSource]
-      if (null != cols_source) {
-        stbl = ctx.lookup_table(col.table.section, col.table.name).map { o =>
-          o.filterKeys { k =>
-            cols_source.columns.length == 0 || cols_source.columns.contains(k)
-          }
-        }
-
-      } else {
-        println("DEBT: only ColumnsTableSource is supported")
-      }
-
-      if (tbl.length == 0) stbl else tbl.map { r0 => stbl.map { r1 => r0 ++ r1 } }.flatten
     }
 
     ctx.retain_table("table", name, combined_tbl)
+  }
+
+  def combine(
+    left: Seq[Map[String, IntrinsicValue]],
+    right: Seq[Map[String, IntrinsicValue]]
+  ): Seq[Map[String, IntrinsicValue]] = left.size match {
+    case 0 => right
+    case _ => left.map { r0 => right.map { r1 => r0 ++ r1 } }.flatten
+  }
+
+  def merge(
+    left: Seq[Map[String, IntrinsicValue]],
+    right: Seq[Map[String, IntrinsicValue]]
+  ): Seq[Map[String, IntrinsicValue]] = left.size match {
+    case 0 => right
+    case _ => (left, right).zipped.map { (rl, rr) => rl ++ rr }
+  }
+
+  def build_table_from_sources(
+    ctx: Context, tr: TableReference, srcs: Seq[TableSource]
+  ): Seq[Map[String, IntrinsicValue]] = {
+    val src_tbl = ctx.lookup_table(tr.section, tr.name)
+    srcs.foldLeft(Seq[Map[String, IntrinsicValue]]()) { (tbl, src) =>
+      // sources are within the SAME table and are the same size, so we zip/merge them together
+      // as if this were an append
+      merge(tbl, src match {
+        case (cols: ColumnsTableSource) => src_tbl.map { r =>
+          r.filterKeys { k => cols.columns.length == 0 || cols.columns.contains(k) }
+        }
+        case (col: ColumnTableSource) => src_tbl.map { r =>
+          Map(col.name -> r(col.source))
+        }
+        case _ => Seq[Map[String, IntrinsicValue]]()
+      })
+    }
   }
 }
--- src/test/scala/org/xalgorithms/rules/steps/AssembleStepSpec.scala
+++ src/test/scala/org/xalgorithms/rules/steps/AssembleStepSpec.scala
@@ -1,62 +1,349 @@
 package org.xalgorithms.rules.steps
 
+import org.scalamock.scalatest.MockFactory
 import org.scalatest._
+
 import org.xalgorithms.rules._
 import org.xalgorithms.rules.elements._
 import org.xalgorithms.rules.steps._
 
-class AssembleStepSpec extends FlatSpec with Matchers {
-  "AssembleStep" should "load tables into the GlobalContext" in {
-    val ctx = new GlobalContext(new ResourceLoadTableSource())
+class AssembleStepSpec extends FlatSpec with Matchers with MockFactory {
+  "AssembleStep" should "load all keys using COLUMNS" in {
+    val ctx = mock[Context]
+    val section = "table"
+    val table_name = "table0"
+    val final_table_name = "table_final"
+    val table = Seq(
+      Map("a" -> new StringValue("00"), "b" -> new StringValue("01")),
+      Map("a" -> new StringValue("10"), "b" -> new StringValue("11")))
 
-    // two simple COLUMNS
-    // ALL columns, no conditions
-    val cols0 = new Column(
-      new TableReference("table", "table0"),
-      Seq(new ColumnsTableSource(Seq(), Seq()))
-    )
-    // only (aa, bb), no conditions
-    val cols1 = new Column(
-      new TableReference("table", "table1"),
-      Seq(new ColumnsTableSource(Seq("aa", "bb"), Seq()))
-    )
+    val cols = new Column(
+      new TableReference(section, table_name),
+      Seq(new ColumnsTableSource(Seq(), Seq())))
 
-    val step = new AssembleStep("table_assemble", Seq(cols0, cols1))
+    val step = new AssembleStep(final_table_name, Seq(cols))
 
-    ctx.load(new PackagedTableReference("package", "table0", "0.0.1", "table0"))
-    ctx.load(new PackagedTableReference("package", "table1", "0.0.1", "table1"))
+    (ctx.lookup_table _).expects(section, table_name).returning(table)
+    (ctx.retain_table _) expects(section, final_table_name, *) onCall { (section, name, tbl) =>
+      tbl.size shouldEqual(table.size)
+      (tbl, table).zipped.foreach { case (rac, rex) =>
+        Seq("a", "b").foreach { k =>
+          rac(k) shouldBe a [StringValue]
+          rac(k).asInstanceOf[StringValue].value shouldEqual(rex(k).asInstanceOf[StringValue].value)
+        }
+      }
+    }
 
     step.execute(ctx)
+  }
 
-    val tbl = ctx.lookup_table("table", "table_assemble")
-    tbl should not be null
-    tbl.length shouldEqual(3)
-    tbl(0)("a") shouldBe a [NumberValue]
-    tbl(0)("a").asInstanceOf[NumberValue].value shouldEqual(1.0)
-    tbl(0)("b") shouldBe a [StringValue]
-    tbl(0)("b").asInstanceOf[StringValue].value shouldEqual("foo")
-    tbl(0)("aa") shouldBe a [NumberValue]
-    tbl(0)("aa").asInstanceOf[NumberValue].value shouldEqual(1.0)
-    tbl(0)("aa") shouldBe a [NumberValue]
-    tbl(0)("bb").asInstanceOf[NumberValue].value shouldEqual(2.0)
-    tbl(0).get("cc") shouldEqual(None)
-    tbl(1)("a") shouldBe a [NumberValue]
-    tbl(1)("a").asInstanceOf[NumberValue].value shouldEqual(2.0)
-    tbl(1)("b") shouldBe a [StringValue]
-    tbl(1)("b").asInstanceOf[StringValue].value shouldEqual("bar")
-    tbl(1)("aa") shouldBe a [NumberValue]
-    tbl(1)("aa").asInstanceOf[NumberValue].value shouldEqual(1.0)
-    tbl(1)("aa") shouldBe a [NumberValue]
-    tbl(1)("bb").asInstanceOf[NumberValue].value shouldEqual(2.0)
-    tbl(1).get("cc") shouldEqual(None)
-    tbl(2)("a") shouldBe a [NumberValue]
-    tbl(2)("a").asInstanceOf[NumberValue].value shouldEqual(3.0)
-    tbl(2)("b") shouldBe a [StringValue]
-    tbl(2)("b").asInstanceOf[StringValue].value shouldEqual("baz")
-    tbl(2)("aa") shouldBe a [NumberValue]
-    tbl(2)("aa").asInstanceOf[NumberValue].value shouldEqual(1.0)
-    tbl(2)("aa") shouldBe a [NumberValue]
-    tbl(2)("bb").asInstanceOf[NumberValue].value shouldEqual(2.0)
-    tbl(2).get("cc") shouldEqual(None)
+  it should "load specific keys using COLUMNS" in {
+    val ctx = mock[Context]
+    val section = "table"
+    val table_name = "table0"
+    val final_table_name = "table_final"
+    val table = Seq(
+      Map("a" -> new StringValue("00"), "b" -> new StringValue("01")),
+      Map("a" -> new StringValue("10"), "b" -> new StringValue("11")))
+    val keys = Seq("a")
+    val skipped_keys = Seq("b")
+
+    val cols = new Column(
+      new TableReference(section, table_name),
+      Seq(new ColumnsTableSource(keys, Seq())))
+
+    val step = new AssembleStep(final_table_name, Seq(cols))
+
+    (ctx.lookup_table _).expects(section, table_name).returning(table)
+    (ctx.retain_table _) expects(section, final_table_name, *) onCall { (section, name, tbl) =>
+      tbl.size shouldEqual(table.size)
+      (tbl, table).zipped.foreach { case (rac, rex) =>
+        keys.foreach { k =>
+          rac(k) shouldBe a [StringValue]
+          rac(k).asInstanceOf[StringValue].value shouldEqual(rex(k).asInstanceOf[StringValue].value)
+        }
+        skipped_keys.foreach { k => rac.contains(k) shouldEqual(false) }
+      }
+    }
+
+    step.execute(ctx)
+  }
+
+  it should "perform a cross-product of multiple COLUMNS from different tables" in {
+    val ctx = mock[Context]
+    val section = "table"
+    val table0_name = "table0"
+    val table1_name = "table1"
+    val final_table_name = "table_final"
+    val table0 = Seq(
+      Map("a" -> new StringValue("00"), "b" -> new StringValue("01")),
+      Map("a" -> new StringValue("10"), "b" -> new StringValue("11"))
+    )
+    val table1 = Seq(
+      Map("c" -> new StringValue("02"), "d" -> new StringValue("03")),
+      Map("c" -> new StringValue("12"), "d" -> new StringValue("13"))
+    )
+    val keys0 = Seq("a")
+    val keys1 = Seq("c", "d")
+
+    val cols0 = new Column(
+      new TableReference(section, table0_name),
+      Seq(
+        new ColumnsTableSource(keys0, Seq())
+      )
+    )
+    val cols1 = new Column(
+      new TableReference(section, table1_name),
+      Seq(
+        new ColumnsTableSource(keys1, Seq())
+      )
+    )
+
+    val step = new AssembleStep(final_table_name, Seq(cols0, cols1))
+
+    (ctx.lookup_table _).expects(section, table0_name).returning(table0)
+    (ctx.lookup_table _).expects(section, table1_name).returning(table1)
+    (ctx.retain_table _) expects(section, final_table_name, *) onCall { (section, name, tbl) =>
+      tbl.size shouldEqual(table0.size * table1.size)
+      table0.indices.foreach { i0 =>
+        table1.indices.foreach { i1 =>
+          val rac = tbl(i0 * table1.size + i1)
+          keys0.foreach { k =>
+            val rex = table0(i0)
+            rac(k) shouldBe a [StringValue]
+            rac(k).asInstanceOf[StringValue].value shouldEqual(rex(k).asInstanceOf[StringValue].value)
+          }
+          keys1.foreach { k =>
+            val rex = table1(i1)
+            rac(k) shouldBe a [StringValue]
+            rac(k).asInstanceOf[StringValue].value shouldEqual(rex(k).asInstanceOf[StringValue].value)
+          }
+        }
+      }
+    }
+
+    step.execute(ctx)
+  }
+
+  it should "merge multiple COLUMNS from the same table" in {
+    val ctx = mock[Context]
+    val section = "table"
+    val table_name = "table0"
+    val final_table_name = "table_final"
+    val table = Seq(
+      Map(
+        "a" -> new StringValue("00"), "b" -> new StringValue("01"),
+        "c" -> new StringValue("02"), "d" -> new StringValue("03")),
+      Map(
+        "a" -> new StringValue("10"), "b" -> new StringValue("11"),
+        "c" -> new StringValue("12"), "d" -> new StringValue("13"))
+    )
+    val keys0 = Seq("a")
+    val keys1 = Seq("c", "d")
+    val skipped_keys = Seq("b")
+
+    val cols = new Column(
+      new TableReference(section, table_name),
+      Seq(
+        new ColumnsTableSource(keys0, Seq()),
+        new ColumnsTableSource(keys1, Seq())
+      )
+    )
+
+    val step = new AssembleStep(final_table_name, Seq(cols))
+
+    (ctx.lookup_table _).expects(section, table_name).returning(table)
+    (ctx.retain_table _) expects(section, final_table_name, *) onCall { (section, name, tbl) =>
+      tbl.size shouldEqual(table.size)
+      (tbl, table).zipped.foreach { case (rac, rex) =>
+        (keys0 ++ keys1).foreach { k =>
+          rac(k) shouldBe a [StringValue]
+          rac(k).asInstanceOf[StringValue].value shouldEqual(rex(k).asInstanceOf[StringValue].value)
+        }
+        skipped_keys.foreach { k => rac.contains(k) shouldEqual(false) }
+      }
+    }
+
+    step.execute(ctx)
+  }
+
+  it should "perform a cross-product of multiple COLUMN from different tables" in {
+    val ctx = mock[Context]
+    val section = "table"
+    val table0_name = "table0"
+    val table1_name = "table1"
+    val final_table_name = "table_final"
+    val table0 = Seq(
+      Map("a" -> new StringValue("00"), "b" -> new StringValue("01")),
+      Map("a" -> new StringValue("10"), "b" -> new StringValue("11"))
+    )
+    val table1 = Seq(
+      Map("c" -> new StringValue("02"), "d" -> new StringValue("03")),
+      Map("c" -> new StringValue("12"), "d" -> new StringValue("13"))
+    )
+    val table0_key = "a"
+    val table1_key = "d"
+
+    val cols0 = new Column(
+      new TableReference(section, table0_name),
+      Seq(
+        new ColumnTableSource(table0_key, table0_key, Seq())
+      )
+    )
+    val cols1 = new Column(
+      new TableReference(section, table1_name),
+      Seq(
+        new ColumnTableSource(table1_key, table1_key, Seq())
+      )
+    )
+
+    val step = new AssembleStep(final_table_name, Seq(cols0, cols1))
+
+    (ctx.lookup_table _).expects(section, table0_name).returning(table0)
+    (ctx.lookup_table _).expects(section, table1_name).returning(table1)
+    (ctx.retain_table _) expects(section, final_table_name, *) onCall { (section, name, tbl) =>
+      tbl.size shouldEqual(table0.size * table1.size)
+      table0.indices.foreach { i0 =>
+        table1.indices.foreach { i1 =>
+          val rac = tbl(i0 * table1.size + i1)
+          val v0 = table0(i0)(table0_key)
+          val v1 = table1(i1)(table1_key)
+
+          rac(table0_key) shouldBe a [StringValue]
+          rac(table0_key).asInstanceOf[StringValue].value shouldEqual(v0.asInstanceOf[StringValue].value)
+
+          rac(table1_key) shouldBe a [StringValue]
+          rac(table1_key).asInstanceOf[StringValue].value shouldEqual(v1.asInstanceOf[StringValue].value)
+        }
+      }
+    }
+
+    step.execute(ctx)
+  }
+
+  it should "merge multiple COLUMN from the same table" in {
+    val ctx = mock[Context]
+    val section = "table"
+    val table_name = "table0"
+    val final_table_name = "table_final"
+    val table = Seq(
+      Map(
+        "a" -> new StringValue("00"), "b" -> new StringValue("01"),
+        "c" -> new StringValue("02"), "d" -> new StringValue("03")),
+      Map(
+        "a" -> new StringValue("10"), "b" -> new StringValue("11"),
+        "c" -> new StringValue("12"), "d" -> new StringValue("13"))
+    )
+    val key0 = "a"
+    val key1 = "d"
+    val skipped_keys = Seq("b", "c")
+
+    val cols = new Column(
+      new TableReference(section, table_name),
+      Seq(
+        new ColumnTableSource(key0, key0, Seq()),
+        new ColumnTableSource(key1, key1, Seq())
+      )
+    )
+
+    val step = new AssembleStep(final_table_name, Seq(cols))
+
+    (ctx.lookup_table _).expects(section, table_name).returning(table)
+    (ctx.retain_table _) expects(section, final_table_name, *) onCall { (section, name, tbl) =>
+      tbl.size shouldEqual(table.size)
+      (tbl, table).zipped.foreach { case (rac, rex) =>
+        Seq(key0, key1).foreach { k =>
+          rac(k) shouldBe a [StringValue]
+          rac(k).asInstanceOf[StringValue].value shouldEqual(rex(k).asInstanceOf[StringValue].value)
+        }
+        skipped_keys.foreach { k => rac.contains(k) shouldEqual(false) }
+      }
+    }
+
+    step.execute(ctx)
+  }
+
+  it should "load a single column using COLUMN" in {
+    val ctx = mock[Context]
+    val section = "table"
+    val table_name = "table0"
+    val final_table_name = "table_final"
+    val table = Seq(
+      Map("a" -> new StringValue("00"), "b" -> new StringValue("01")),
+      Map("a" -> new StringValue("10"), "b" -> new StringValue("11")))
+    val sk = "b"
+    val tk = "bbb"
+
+    val cols = new Column(
+      new TableReference(section, table_name),
+      Seq(new ColumnTableSource(tk, sk, Seq())))
+
+    val step = new AssembleStep(final_table_name, Seq(cols))
+
+    (ctx.lookup_table _).expects(section, table_name).returning(table)
+    (ctx.retain_table _) expects(section, final_table_name, *) onCall { (section, name, tbl) =>
+      tbl.size shouldEqual(table.size)
+      (tbl, table).zipped.foreach { case (rac, rex) =>
+        rac(tk) shouldBe a [StringValue]
+        rac(tk).asInstanceOf[StringValue].value shouldEqual(rex(sk).asInstanceOf[StringValue].value)
+      }
+    }
+
+    step.execute(ctx)
+  }
+
+  it should "combine COLUMNS with additional COLUMN using a merge" in {
+    val ctx = mock[Context]
+    val section = "table"
+    val final_table_name = "table_final"
+    val table0_name = "table0"
+    val table0 = Seq(
+      Map("a" -> new StringValue("00"), "b" -> new StringValue("01")),
+      Map("a" -> new StringValue("10"), "b" -> new StringValue("11")),
+      Map("a" -> new StringValue("20"), "b" -> new StringValue("21"))
+    )
+    val table1_name = "table1"
+    val t1k0 = "x"
+    val t1k1 = "y"
+    val table1 = Seq(
+      Map("x" -> new StringValue("x0"), "y" -> new StringValue("y0")),
+      Map("x" -> new StringValue("x1"), "y" -> new StringValue("y1"))
+    )
+      
+    val table0_col = new Column(
+      new TableReference(section, table0_name),
+      Seq(new ColumnsTableSource(Seq(), Seq()))
+    )
+    val table1_col = new Column(
+      new TableReference(section, table1_name),
+      Seq(
+        new ColumnTableSource(t1k0, t1k0, Seq()),
+        new ColumnTableSource(t1k1, t1k1, Seq())
+      )
+    )
+
+    val step = new AssembleStep(final_table_name, Seq(table0_col, table1_col))
+
+    (ctx.lookup_table _).expects(section, table0_name).returning(table0)
+    (ctx.lookup_table _).expects(section, table1_name).returning(table1)
+
+    (ctx.retain_table _) expects(section, final_table_name, *) onCall { (section, name, tbl) =>
+      tbl.size shouldEqual(table0.size * table1.size)
+      table0.indices.foreach { i0 =>
+        table1.indices.foreach { i1 =>
+          val rac = tbl(2 * i0 + i1)
+          Seq(t1k0, t1k1).foreach { k =>
+            rac(k) shouldBe a [StringValue]
+            rac(k).asInstanceOf[StringValue].value shouldEqual(table1(i1)(k).asInstanceOf[StringValue].value)
+          }
+          table0(i0).foreach { case (k0, v0) =>
+            rac(k0) shouldBe a [StringValue]
+            rac(k0).asInstanceOf[StringValue].value shouldEqual(v0.asInstanceOf[StringValue].value)
+          }
+        }
+      }
+    }
+
+    step.execute(ctx)
   }
 }
