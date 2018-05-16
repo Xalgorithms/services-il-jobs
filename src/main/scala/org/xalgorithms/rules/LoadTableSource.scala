13120aeb434dc5a571bb59ee229fda6c808337f2
Don Kelly <karfai@gmail.com>
2018-05-16 15:59:02 +0400

    Remove interpreter code to a new library
    Connects to #51
    
    Change-Id: I2d20c246da5b74ff62ebb383f13638d4862f2acb

--- src/main/scala/org/xalgorithms/rules/LoadTableSource.scala
+++
@@ -1,9 +0,0 @@
-package org.xalgorithms.rules
-
-import org.xalgorithms.rules.elements.{ IntrinsicValue, PackagedTableReference }
-
-import play.api.libs.json._
-
-abstract class LoadTableSource {
-  def load(ptref: PackagedTableReference): Seq[Map[String, IntrinsicValue]]
-}
--- src/main/scala/org/xalgorithms/rules/steps/AssignmentStep.scala
+++
@@ -1,7 +0,0 @@
-package org.xalgorithms.rules.steps
-
-import org.xalgorithms.rules.elements.{ Assignment, TableReference }
-
-abstract class AssignmentStep(val table: TableReference, val assignments: Seq[Assignment]) extends Step {
-}
-
