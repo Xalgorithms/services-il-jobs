ce8cdbc1e2d09f7160efcef1dfeca826170e020b
Don Kelly <karfai@gmail.com>
2018-10-19 12:48:53 +0400

    Updates the ApplicableRules job to match current schemas

--- src/main/scala/org/xalgorithms/jobs/ApplicableRules.scala
+++ src/main/scala/org/xalgorithms/jobs/ApplicableRules.scala
@@ -34,6 +34,8 @@ import com.mongodb.spark.MongoSpark
 import org.apache.spark.rdd.RDD
 import org.apache.spark.streaming.dstream._
 import org.bson._
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
 
 class DocumentValue(val doc: BsonDocument, val section: String, val key: String) extends Serializable {
   def find(): Seq[BsonValue] = section match {
@@ -63,7 +65,7 @@ class DocumentValue(val doc: BsonDocument, val section: String, val key: String)
 
 object DocumentValue {
   def apply(doc: BsonDocument, section: String, key: String): DocumentValue = {
-    new DocumentValue(doc, section, key)
+    new DocumentValue(doc.getDocument("content", new BsonDocument()), section, key)
   }
 }
 
@@ -84,17 +86,35 @@ class ApplicableRules(cfg: ApplicationConfig) extends KafkaSparkStreamingApplica
 
   def execute(): Unit = {
     with_context(cfg, { (ctx, sctx, events, input) =>
-      // 1. build a paired stream from the MongoDB, using "public_id" as the key:
+      // 1. build a paired stream from the MongoDB, using "public_id" as the key
+      // TODO: this needs to be early combined with the incoming document ids stream to
+      // filter it
       val docs_stream = new ConstantInputDStream(sctx, MongoSpark.load[BsonDocument](ctx))
-        .map(doc => doc.getString("public_id").getValue() -> doc)
+        .map { doc =>
+          val public_id = doc.getString("public_id").getValue()
+          events.value.info(
+            "loading document",
+            Map("public_id" -> public_id)
+          )
+
+          public_id -> doc
+        }
       // => ((public_id), (BsonDocument)
 
       // 2. From the incoming input stream ("{document_id}:{rule_id}"), build a paired
       // stream using document_id as the key. This will allow a join with the stream of
       // documents coming from MongoDB
-      val ids_stream = input.map { doc_rule_id =>
-        val ids = doc_rule_id.split(":")
-        ids(0) -> ids(1)
+      val ids_stream = input.map { s =>
+        Json.parse(s)
+      }.map { o =>
+        val doc_id = (o \ "args" \ "document_id").asOpt[String].getOrElse(null)
+        val rule_id = (o \ "args" \ "rule_id").asOpt[String].getOrElse(null)
+
+        events.value.info(
+          "extracting ids from incoming JSON",
+          Map("document_id" -> doc_id, "rule_id" -> rule_id)
+        )
+        doc_id -> rule_id
       }
       // => ((document_id), (rule_id))
 
@@ -128,6 +148,10 @@ class ApplicableRules(cfg: ApplicationConfig) extends KafkaSparkStreamingApplica
           val section = tup._2.getString("section")
           val key = tup._2.getString("key")
 
+          events.value.info(
+            "building document value",
+            Map("section" -> section, "key" -> key)
+          )
           s"${section}:${key}" -> Tuple3(tup._1._1, tup._1._2, DocumentValue(tup._1._3, section, key))
         }
       // => ((section:key), (doc_id, rule_id, DocumentValue)
@@ -144,6 +168,10 @@ class ApplicableRules(cfg: ApplicationConfig) extends KafkaSparkStreamingApplica
         val key = cr.getString("key")
         val rule_id = cr.getString("rule_id")
 
+        events.value.info(
+          "building whens maps",
+          Map("section" -> section, "key" -> key)
+        )
         s"${section}:${key}" -> Tuple2(rule_id, ApplyOperator(cr.getString("op"), cr.getString("val")))
       }
       // => ((section:key), (rule_id, ApplyOperator))
@@ -162,18 +190,62 @@ class ApplicableRules(cfg: ApplicationConfig) extends KafkaSparkStreamingApplica
         // filter on matching rule ids -- tup._1 has the rule_id
         // from the input stream; tup._2 from the whens table
         .filter(tup => tup._1._2 == tup._2._1)
-        .map(tup => s"${tup._1._1}:${tup._1._2}" -> tup._2._2.matches_any(tup._1._3.find()))
-        .reduceByKey((a: Boolean, b: Boolean) => a && b)
-        .filter(_._2)
-        .map(_._1)
+        .map { tup =>
+          val found = tup._1._3.find()
+          val matches_any = tup._2._2.matches_any(found)
+          events.value.info(
+            "found results in document value",
+            Map(
+              "section" -> tup._1._3.section,
+              "key" -> tup._1._3.key,
+              "found" -> found.map { v => v.toString }.mkString("; "),
+              "matches_any" -> matches_any.toString,
+              "doc" -> tup._1._3.doc.toString
+            )
+          )
+          // for reduceByKey, which isn't working
+          //s"${tup._1._1}:${tup._1._2}" -> matches_any
+          Tuple3(tup._1._1, tup._1._2, matches_any)
+        }
+        // .reduceByKey((a: Boolean, b: Boolean) => {
+        //   events.value.info(
+        //     "reducing repeated keys",
+        //     Map("a" -> a.toString, "b" -> b.toString)
+        //   )
+        //   a && b
+        // })
+        .filter { tup =>
+          events.value.info(
+            "filtering stream",
+            Map("document_id" -> tup._1, "rule_id" -> tup._2, "match" -> tup._3.toString)
+          )
+          tup._3
+        }
+        .map { tup =>
+          events.value.gave("delivering", Map("document_id" -> tup._1, "rule_id" -> tup._2))
+          Json.obj(
+            "context" -> Map("task" -> "compute", "action" -> "applicable"),
+            "args" -> Map("document_id" -> tup._1, "rule_id" -> tup._2)
+          ).toString          
+        }
       // => "document_id:rule_id"
     })
   }
 }
 
 object ApplicableRules {
-  def main(args: Array[String]) : Unit = {
-    val job = new ApplicableRules(ApplicationConfig("ApplicableRules"))
+  def execute_job(name: String): Unit = {
+    val job = new ApplicableRules(ApplicationConfig(name))
     job.execute()
+  }
+
+  def main(args: Array[String]) : Unit = {
+    execute_job("ApplicableRules")
+  }
+}
+
+object ValidateApplicableRules {
+  def main(args: Array[String]) : Unit = {
+    ApplicableRules.execute_job("ValidateApplicableRules")
   }
 }
