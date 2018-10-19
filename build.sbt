d3368cb1ebd40b3bc2105a4f13c8b76e63650441
Don Kelly <karfai@gmail.com>
2018-10-19 12:48:41 +0400

    Upgrades Mongo/Spark to 2.3.1

--- build.sbt
+++ build.sbt
@@ -25,7 +25,7 @@ lazy val VERSION_SCALA               = "2.11.11"
 lazy val VERSION_SPARK               = "2.3.0"
 lazy val VERSION_CASSANDRA_CONNECTOR = "2.3.1-s_2.11"
 // NOTE: Upgrade (don't forget deployment scripts)
-lazy val VERSION_MONGO_CONNECTOR     = "2.2.0"
+lazy val VERSION_MONGO_CONNECTOR     = "2.3.1"
 lazy val VERSION_NSCALA_TIME         = "2.18.0"
 lazy val VERSION_TYPESAFE_CONFIG     = "1.2.1"
 lazy val VERSION_FICUS               = "1.1.1"
--- local-spark-submit.sh
+++ local-spark-submit.sh
@@ -26,4 +26,4 @@ echo "> attempting: $1"
 OPTS=`cat $1.opts.development`
 echo ">   additional opts: $OPTS"
 
-spark-submit --class "org.xalgorithms.jobs.$1" --packages datastax:spark-cassandra-connector:2.3.1-s_2.11,com.typesafe:config:1.3.1,org.apache.spark:spark-streaming-kafka-0-10_2.11:2.3.0,net.ceedubs:ficus_2.11:1.1.1,org.mongodb.spark:mongo-spark-connector_2.11:2.2.0,org.gnieh:diffson-play-json_2.11:2.2.1 $OPTS target/scala-2.11/jobs-spark-il_2.11-0.1.2.jar
+spark-submit --class "org.xalgorithms.jobs.$1" --packages datastax:spark-cassandra-connector:2.3.1-s_2.11,com.typesafe:config:1.3.1,org.apache.spark:spark-streaming-kafka-0-10_2.11:2.3.0,net.ceedubs:ficus_2.11:1.1.1,org.mongodb.spark:mongo-spark-connector_2.11:2.3.1,org.gnieh:diffson-play-json_2.11:2.2.1 $OPTS target/scala-2.11/jobs-spark-il_2.11-0.1.2.jar
